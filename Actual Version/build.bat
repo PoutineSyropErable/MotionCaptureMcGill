8f3bf25a10dd68a8992aff7effc3162673224845
motioncapture <motioncapturelab@gmail.com>
2024-08-19 16:33:34 -0400

    Before removing the VS studio files

---
+++ Actual Version/build.bat
@@ -0,0 +1,18 @@
+
+@echo off
+REM Set up directories
+set INCLUDE_DIR=include
+set SRC_DIR=.
+set LIB_DIR=lib
+
+REM Compile with cl.exe
+cl /EHsc /Fe:MinimalClient.exe %SRC_DIR%\MinimalClient.cpp /I%INCLUDE_DIR% /link /LIBPATH:%LIB_DIR% NatNetLib.lib
+
+REM Add LIB_DIR to PATH for DLL loading
+set PATH=%PATH%;%CD%\%LIB_DIR%
+
+REM Run the application
+MinimalClient.exe
+
+REM Pause the Command Prompt to see the output
+@pause
